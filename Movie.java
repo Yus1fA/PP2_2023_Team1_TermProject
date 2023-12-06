ef008f26f0db125c91ef350d77cf13f257233740
Zamin Ashurov <zashurov18125@ada.edu.az>
2023-12-06 23:57:53 +0400

    added exception handling

---
+++ Movie.java
@@ -0,0 +1,53 @@
+class Movie {
+    private String title;
+    private String director;
+    private int releaseYear;
+    private int runningTime;
+
+    public Movie(String title, String director, int releaseYear, int runningTime) {
+        this.title = title;
+        this.director = director;
+        this.releaseYear = releaseYear;
+        this.runningTime = runningTime;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    void setTitle(String title) {
+        this.title = title;
+    }
+
+    String getDirector() {
+        return director;
+    }
+
+    void setDirector(String director) {
+        this.director = director;
+    }
+
+    int getReleaseYear() {
+        return releaseYear;
+    }
+
+    void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    int getRunningTime() {
+        return runningTime;
+    }
+
+    void setRunningTime(int runningTime) {
+        this.runningTime = runningTime;
+    }
+
+   
+    displayMovieDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Director: " + director);
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Running Time: " + runningTime + " minutes");
+    }
+}
--- MovieDatabase.java
+++ MovieDatabase.java
@@ -3,29 +3,43 @@ import java.util.Map;
 
 public class MovieDatabase {
     private Map<String, Movie> movies;
+    private Map<String, UserClass> users;
 
     public MovieDatabase() {
         movies = new HashMap<>();
+        users = new HashMap<>();
     }
 
     public void addMovie(String title, String director, int releaseYear, int runningTime) {
-        String key = generateKey(title, director, releaseYear);
-        if (!movies.containsKey(key)) {
-            Movie newMovie = new Movie(title, director, releaseYear, runningTime);
-            movies.put(key, newMovie);
-            System.out.println("Movie added: " + newMovie.getTitle() + " (" + newMovie.getReleaseYear() + ")");
-        } else {
-            System.out.println("Movie already exists in the database.");
+        try {
+            validateMovieInput(title, director, releaseYear, runningTime);
+
+            String key = generateKey(title, director, releaseYear);
+            if (!movies.containsKey(key)) {
+                Movie newMovie = new Movie(title, director, releaseYear, runningTime);
+                movies.put(key, newMovie);
+                System.out.println("Movie added: " + newMovie.getTitle() + " (" + newMovie.getReleaseYear() + ")");
+            } else {
+                System.out.println("Movie already exists in the database.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error adding movie: " + e.getMessage());
         }
     }
 
     public void removeMovie(String title, String director, int releaseYear) {
-        String key = generateKey(title, director, releaseYear);
-        if (movies.containsKey(key)) {
-            Movie removedMovie = movies.remove(key);
-            System.out.println("Movie removed: " + removedMovie.getTitle() + " (" + removedMovie.getReleaseYear() + ")");
-        } else {
-            System.out.println("Movie not found in the database.");
+        try {
+            validateMovieInput(title, director, releaseYear, 0);
+
+            String key = generateKey(title, director, releaseYear);
+            if (movies.containsKey(key)) {
+                Movie removedMovie = movies.remove(key);
+                System.out.println("Movie removed: " + removedMovie.getTitle() + " (" + removedMovie.getReleaseYear() + ")");
+            } else {
+                System.out.println("Movie not found in the database.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error removing movie: " + e.getMessage());
         }
     }
 
@@ -38,21 +52,34 @@ public class MovieDatabase {
         return title + "_" + director + "_" + releaseYear;
     }
 
-    // For testing
-    public static void main(String[] args) {
-        MovieDatabase movieDatabase = new MovieDatabase();
-
-        // Adding movies
-        movieDatabase.addMovie("Inception", "Christopher Nolan", 2010, 148);
-        movieDatabase.addMovie("The Shawshank Redemption", "Frank Darabont", 1994, 142);
-
-        // Retrieving and printing movie details
-        Movie movie = movieDatabase.getMovie("Inception", "Christopher Nolan", 2010);
-        if (movie != null) {
-            System.out.println("Movie details: " + movie.getTitle() + " (" + movie.getReleaseYear() + ")");
+    private void validateMovieInput(String title, String director, int releaseYear, int runningTime) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be null or empty.");
         }
 
-        // Removing a movie
+        if (director == null || director.isEmpty()) {
+            throw new IllegalArgumentException("Director cannot be null or empty.");
+        }
+
+        if (releaseYear <= 0) {
+            throw new IllegalArgumentException("Release year must be a positive value.");
+        }
+
+        if (runningTime < 0) {
+            throw new IllegalArgumentException("Running time must be a non-negative value.");
+        }
+    }
+
+    // Other methods for user and database management...
+
+    public static void main(String[] args) {
+        MovieDatabase movieDatabase = new MovieDatabase();
+        movieDatabase.addMovie("Inception", "Christopher Nolan", 2010, 148);
+        movieDatabase.addMovie("The Shawshank Redemption", "Frank Darabont", 1994, 142);
+        Movie movie = movieDatabase.getMovie("Inception", "Christopher Nolan", 2010);
+        if (movie != null) {
+            movie.displayMovieDetails();
+        }
         movieDatabase.removeMovie("The Shawshank Redemption", "Frank Darabont", 1994);
     }
 }
