a1e6a99707ede3eacec3460db8aece9cf58a834c
Yusif Ashrafov 18062 <yashrafov18062@ada.edu.az>
2023-12-10 22:48:09 +0400

    Panel improvements from the outside perspective

--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,38 @@
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class UserInterface extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setTitle("Final Project");
+
+        JLabel welcome = new JLabel("Welcome to Movie Database!");
+        frame.add(welcome, BorderLayout.NORTH);
+
+        JTabbedPane tabbed = new JTabbedPane();
+
+        JPanel browse= new JPanel(new BorderLayout());
+        JScrollPane movieList = new JScrollPane(new JList<>());
+
+        movieList.setBorder(BorderFactory.createTitledBorder("Movies"));
+        browse.add(movieList, BorderLayout.CENTER);
+        tabbed.addTab("Browse Movies", browse);
+
+        JPanel watchlist = new JPanel(new BorderLayout());
+        JScrollPane watchlistScroll = new JScrollPane(new JList<>());
+
+        watchlistScroll.setBorder(BorderFactory.createTitledBorder("Watchlist"));
+        watchlist.add(watchlistScroll, BorderLayout.CENTER);
+        tabbed.addTab("Watchlist", watchlist);
+
+        frame.add(tabbed, BorderLayout.CENTER);
+
+        JButton addWatchlist = new JButton("Add to Watchlist");
+        watchlist.add(addWatchlist, BorderLayout.SOUTH);
+
+        JButton removeWatchlist = new JButton("Remove from Watchlist");
+        watchlist.add(removeWatchlist, BorderLayout.NORTH);
+
+        frame.setTitle("Movie Database");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(420, 420);
